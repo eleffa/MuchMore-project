2c3ca90baaeffe38278d28cc714a3ab8c1a25dec
Emma Effa <eleffa@yahoo.com>
2024-12-09 20:27:33 +0100

    Update interactive_ihm.py

--- interactive_ihm.py
+++ interactive_ihm.py
@@ -10,25 +10,22 @@ def run():
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to MuchMore-project! 👋")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a page above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        ### MuchMore-project
+        This dataset consists of abstracts from medical scientific publications, 
+        covering various fields such as Cardiology, Ophthalmology, etc. Therefore,
+        we are dealing with a multiclass classification problem (assigning a single 
+        possible class to a document). The category to which a document belongs 
+        corresponds to the first part of its name.
+
+        **👈 Select a page from the sidebar** to see the dashboard
+        or to play with the interactive exploration of data !
+        
     """
     )
 
