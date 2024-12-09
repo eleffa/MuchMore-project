5064a1dc0c8782efe79dc1df2b3b726cc700341f
Emma Effa <eleffa@yahoo.com>
2024-12-09 21:04:29 +0100

    Update interactive_ihm.py

--- interactive_ihm.py
+++ interactive_ihm.py
@@ -52,7 +52,7 @@ def load_data_from_github(url: str):
 
 
 
-df = load_data("/mount/src/muchmore-project/MuchMoreData.csv")
+df = load_data("https://github.com/eleffa/MuchMore-project/blob/main/MuchMoreData.csv")
 
 # Accueil
 if page == "Accueil":
