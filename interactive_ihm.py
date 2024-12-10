6ebd7ab59ba7c1735e8fe8e8c93126e6d5ae86ca
Emma Effa <eleffa@yahoo.com>
2024-12-10 17:22:37 +0100

    Update interactive_ihm.py

--- interactive_ihm.py
+++ interactive_ihm.py
@@ -195,7 +195,6 @@ elif page == "Exploration Interactive":
     st.write("Exemples de documents mal classés :")
     for i, doc in enumerate(misclassified[:5]):
         st.write(f"**Document {i+1} :** {tfidf.inverse_transform(doc)}")
-        st.write(f"**Document {i+1} : **{df.iloc[idx]['content']}")
         st.write(f"**Classe réelle :** {y_test.iloc[i]} | **Classe prédite :** {y_pred[i]}")
 
     # Visualisation des performances
